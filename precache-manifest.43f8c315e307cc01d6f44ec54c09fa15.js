40aadf2414fb1d5ce22f3b7eb55d57a193d26cec
alexanderman <alexander.shnirman@gmail.com>
2020-07-05 15:44:22 +0300

    Updates

---
+++ precache-manifest.43f8c315e307cc01d6f44ec54c09fa15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c366494e3410f9d39df091bbb68ca323",
+    "url": "/public_chat-react_client/index.html"
+  },
+  {
+    "revision": "70ca6258eccc481b15f3",
+    "url": "/public_chat-react_client/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "9e201d0d29afba64ca28",
+    "url": "/public_chat-react_client/static/css/main.079925a7.chunk.css"
+  },
+  {
+    "revision": "70ca6258eccc481b15f3",
+    "url": "/public_chat-react_client/static/js/2.bf46f4ff.chunk.js"
+  },
+  {
+    "revision": "b0108ac9c62216c02af456fb85c881a1",
+    "url": "/public_chat-react_client/static/js/2.bf46f4ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e201d0d29afba64ca28",
+    "url": "/public_chat-react_client/static/js/main.41584630.chunk.js"
+  },
+  {
+    "revision": "11f1d55c09e092b90a93",
+    "url": "/public_chat-react_client/static/js/runtime-main.802762ca.js"
+  }
+]);
